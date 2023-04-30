151a5e8cf1c502f60aa5ed5b920e1e32a428cff3
Yuki Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2023-04-30 13:26:45 +0900

    update キー操作でアニメーションを変更できるように調整

--- src/utils/CharacterControls.ts
+++ src/utils/CharacterControls.ts
@@ -1,5 +1,13 @@
-import { AnimationAction, AnimationMixer, Camera, Group } from "three";
+import {
+  AnimationAction,
+  AnimationMixer,
+  Camera,
+  Group,
+  Quaternion,
+  Vector3,
+} from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { DIRECTIONS } from "./KeyCode";
 
 export class CharacterControls {
   model: Group;
@@ -7,8 +15,21 @@ export class CharacterControls {
   animationsMap: Map<string, AnimationAction> = new Map();
   orbitControl: OrbitControls;
   camera: Camera;
+
+  // state
   toggleRun: boolean = true;
   currentAction: string;
+
+  // temporary data
+  walkDirection = new Vector3();
+  rotateAngle = new Vector3(0, 1, 0);
+  rotateQuartenion: Quaternion = new Quaternion();
+  cameraTarget = new Vector3();
+
+  // constants
+  fadeDuration: number = 0.2;
+  runVelocity = 5;
+  walkVelocity = 2;
 
   constructor(
     model: Group,
@@ -36,6 +57,26 @@ export class CharacterControls {
   }
 
   update(delta: number, keypressed: any) {
+    const directionPressed = DIRECTIONS.some((key) => keypressed[key] === true);
+    let play = "";
+    if (directionPressed && this.toggleRun) {
+      play = "Run";
+    } else if (directionPressed) {
+      play = "Walk";
+    } else {
+      play = "Idle";
+    }
+
+    if (this.currentAction !== play) {
+      const toPlay = this.animationsMap.get(play);
+      const current = this.animationsMap.get(this.currentAction);
+
+      current?.fadeOut(this.fadeDuration);
+      toPlay?.reset().fadeIn(this.fadeDuration).play();
+
+      this.currentAction = play;
+    }
+
     this.mixer.update(delta);
   }
 }
